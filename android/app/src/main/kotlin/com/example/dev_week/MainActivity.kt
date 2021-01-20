88f3c62a597e7d3f8f7bf6a36ba25ef608391ebd
maanas-talwar <maanastalwar1@gmail.com>
2021-01-20 19:43:40 +0530

    Base project

---
+++ android/app/src/main/kotlin/com/example/dev_week/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dev_week
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List<Person> allPeople;
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    allPeople = [Person("Maanas", "Why so serious?")];
+    allPeople.add(Person("Shubh", "Alright, Alright, Alright!"));
+//    allPeople.add(Person("Shubh", "Why so serious?"));
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xff073642),
+            title: Center(
+              child: Text(
+                "IOSD Family",
+                style: TextStyle(color: Colors.white, fontSize: 35),
+              ),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: GridView.builder(
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: allPeople.length,
+              itemBuilder: (BuildContext context, int i) => Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AutoSizeText(
+                        allPeople[i].name,
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 21,
+                        ),
+                        minFontSize: 10,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      AutoSizeText(
+                        allPeople[i].quote,
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                        minFontSize: 10,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class Person {
+  String name;
+  String quote;
+
+  Person(String a, String b) {
+    this.name = a;
+    this.quote = b;
+  }
+}
